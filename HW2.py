e803984f237c357952d224b248edd7fc15bb991a
KunHeeLee7 <112879165+KunHeeLee7@users.noreply.github.com>
2026-05-10 11:41:22 +0900

    Add comment
    Add comment

--- HW2.py
+++ HW2.py
@@ -1,11 +1,17 @@
 # HW2
+
 from gpiozero import Button, LED
 from signal import pause
 
+# GPIO setup
 led = LED(14)
 button = Button(4)
 
+# Turn on LED when button is pressed
 button.when_pressed = led.on
+
+# Turn off LED when button is released
 button.when_released = led.off
 
+# Keep program running
 pause()
